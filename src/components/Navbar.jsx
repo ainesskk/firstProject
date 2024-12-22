0e3797639eb45acfb03714e5b61bdb6027d941fa
ainesk <ksenia_krr@mail.ru>
2024-12-22 17:53:30 +0300

    add navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,22 @@
-import "../styles/Navbar.module.css";
+import styles from "../styles/Navbar.module.css";
 
 export default function Navbar() {
     return (
-        <div className="flex items-center justify-between border-b-2 w-full">
-            <div className="mr-4">
-                <img src="src/assets/Logo.png" alt="logo" />
-            </div>
-            <div className="flex space-x-4">
-                <p className="text-lavender">Main</p>
-                <p className="text-greyDark">About</p>
-                <p className="text-greyDark">Get in touch</p>
+        <div className="border-b-2 w-full pt-5 pb-5">
+            <div className="main-container flex items-center justify-between">
+                <div className="mr-4">
+                    <img src="src/assets/Logo.png" alt="logo"/>
+                </div>
+                <div className="flex space-x-6;">
+                    <div className={styles.navbarPoints}>
+                        <p className="text-lavender">Main</p>
+                        <p className="text-greyDark">About</p>
+                        <p className="text-greyDark">Get in touch</p>
+                    </div>
+                    <div className={styles.burgerMenu}>
+                        <img src="src/assets/menu.png" alt="logo"/>
+                    </div>
+                </div>
             </div>
         </div>
     );
